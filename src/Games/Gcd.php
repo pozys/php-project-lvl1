be0be8dcfbeb605d1512ef9a1856792c92ce1243
Культяпов Константин <pozys@mail.ru>
2022-05-20 21:36:19 +0300

    update gcd

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -16,12 +16,26 @@ function play()
     $userName = getUserName();
     printRules('Find the greatest common divisor of given numbers.');
 
-    $questions = getQuestions();
-    $answers = getAnswers($questions);
+    [$questions, $answers] = getQuestionsAnswers();
 
     $isWinner = getGameResult($questions, $answers, $userName);
 
     sayGoodbye($isWinner, $userName);
+}
+
+function getQuestionsAnswers(): array
+{
+    $questions = [];
+    $answers = [];
+
+    for ($i = 0, $rounds = getRoundCount(); $i < $rounds; $i++) {
+        $arg1 = rand(0, 100);
+        $arg2 = rand(0, 100);
+        $questions[] = implode(getSeparator(), [$arg1, $arg2]);
+        $answers[] = getRightAnswer($arg1, $arg2);
+    }
+
+    return [$questions, $answers];
 }
 
 function getQuestions(): array
@@ -37,21 +51,8 @@ function getQuestions(): array
     return $questions;
 }
 
-function getAnswers(array $questions): array
+function getRightAnswer(int $arg1, int $arg2)
 {
-    $answers = [];
-
-    foreach ($questions as $question) {
-        $answers[] = getRightAnswer($question);
-    }
-
-    return $answers;
-}
-
-function getRightAnswer(string $question)
-{
-    [$arg1, $arg2] = explode(getSeparator(), $question);
-
     return nod($arg1, $arg2);
 }
 
