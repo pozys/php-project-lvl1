fde782f50d3c96612b068d522132971a860c8168
Культяпов Константин <pozys@mail.ru>
2022-05-24 06:16:45 +0300

    fix bugs

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -7,40 +7,21 @@ use function Php\Project\Lvl1\Engine\{runGame, getRoundCount};
 function play()
 {
     $rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-    $questions = getQuestions();
-    $answers = getAnswers($questions);
-
-    runGame($answers, $questions, $rules);
-}
-
-function getQuestions(): array
-{
     $questions = [];
+    $answers = [];
+
     $minNumber = 0;
     $maxNumber = 100;
 
     for ($i = 0, $rounds = getRoundCount(); $i < $rounds; $i++) {
-        $questions[] = rand($minNumber, $maxNumber);
+        $question = rand($minNumber, $maxNumber);
+        $questions[] = $question;
+
+        $primes = getSuitablePrimes($question);
+        $answers[] = isPrime($question, $primes) ? 'yes' : 'no';
     }
 
-    return $questions;
-}
-
-function getAnswers(array $questions): array
-{
-    $answers = [];
-
-    foreach ($questions as $question) {
-        $answers[] = getRightAnswer($question);
-    }
-
-    return $answers;
-}
-
-function getRightAnswer(int $question): string
-{
-    $primes = getSuitablePrimes($question);
-    return isPrime($question, $primes) ? 'yes' : 'no';
+    runGame($answers, $questions, $rules);
 }
 
 function getSuitablePrimes(int $number, array $primes = []): array
